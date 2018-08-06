e2770158bbf08db7c477340a4f79af84b8d423aa
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-06 09:15:07 -0300

    refatoração da classe para Cria... e adiçao da classe Insere

--- banco/src/main/java/jpa/InsereConta.java
+++ banco/src/main/java/jpa/InsereConta.java
@@ -6,28 +6,27 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class TesteConta {
-
-    public static void main(String[] args){
-
-
-       /* Conta conta = new Conta();
+public class InsereConta {
+    public static void main (String[] args){
+        Conta conta  = new Conta();
         conta.setAgencia("1234");
         conta.setBanco("1234");
-        conta.setId(1235);
         conta.setNumero("1234");
-        conta.setTitular("victor");
-*/
+        conta.setTitular("Victor Almeida");
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("conta");
         EntityManager em = emf.createEntityManager();
-      //  em.getTransaction().begin();
-        //em.persist(conta);
-       //  em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        em.persist(conta);
+        em.getTransaction().commit();
+
+        System.out.println("ID da tarefa:  "+ conta.getId());
+
         em.close();
         emf.close();
 
 
     }
-
 
 }
