708b959b8f75fd892b6ac49b3c4dc3215967fd06
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-06 10:21:25 -0300

    adição de codigo na BuscaContas

--- banco/src/main/java/connection/JpaUtil.java
+++ banco/src/main/java/connection/JpaUtil.java
@@ -4,6 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Serve para retornar uma instancia do entityManager pra que nao seja instanciado toda vez em cada classe
+ * E tambem com o metodo para fechar a conexão
+ */
 public class JpaUtil {
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("conta");
 
--- banco/src/main/java/jpa/BuscaContas.java
+++ banco/src/main/java/jpa/BuscaContas.java
@@ -1,16 +1,30 @@
 package jpa;
 
 import connection.JpaUtil;
+import conta.Conta;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Aqui as entitdades se encontram no estado managed ou seja elas tem sincronização automatica com o banco
+ */
 public class BuscaContas {
 
     public static void main(String[] args){
 
         EntityManager em = new JpaUtil().getEntityManager();
+        em.getTransaction().begin();
+
+        Conta conta = em.find(Conta.class,6);
+
+        //conta.setTitular("Jaison");
+
+        System.out.println(conta.getTitular());
+
+        em.getTransaction().commit();
+
 
         em.close();
         JpaUtil.closeEMF();
