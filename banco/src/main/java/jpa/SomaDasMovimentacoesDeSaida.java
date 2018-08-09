7367e4db576df0a35e32fe3a7540c41f8371b731
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-09 15:48:34 -0300

    Mudança para realizar a media

--- banco/src/main/java/jpa/SomaDasMovimentacoesDeSaida.java
+++ banco/src/main/java/jpa/SomaDasMovimentacoesDeSaida.java
@@ -17,12 +17,22 @@ public class SomaDasMovimentacoesDeSaida {
         conta.setId(2);
 
         String jpql ="select sum(m.valor) from Movimentacao m where m.conta = :pConta"+ " and m.tipo = :pTipo"+" order by m.valor desc";
+        /**
+         * Esta Query faz a media de todos os valores
+         * String jpql ="select avg(m.valor) from Movimentacao m where m.conta = :pConta"+ " and m.tipo = :pTipo"+" order by m.valor desc";
+         */
+
 
         Query query = em.createQuery(jpql);
         query.setParameter("pConta",conta);
         query.setParameter("pTipo", TipoMovimentacao.SAIDA);
 
         BigDecimal soma = (BigDecimal) query.getSingleResult();
+        /**
+         * Double soma =(Double)query.getSingleResult();
+         * caso necessario descomentar essa linha e a da Query acima para realizar a media
+         */
+
         System.out.println("A soma é: "+ soma);
 
         em.getTransaction().commit();
