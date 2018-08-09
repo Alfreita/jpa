945a709e508e9f5d4fca9d044e2f631b2cc21ef5
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-09 10:59:10 -0300

    Correção de erros na popula conta

--- banco/src/main/java/popula/PopulaConta.java
+++ banco/src/main/java/popula/PopulaConta.java
@@ -14,40 +14,40 @@ public class PopulaConta {
         conta.setTitular("victor");
 
         Conta conta1  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("heloisa");
+        conta1.setAgencia("1234");
+        conta1.setBanco("Itau");
+        conta1.setNumero("1234");
+        conta1.setTitular("heloisa");
 
         Conta conta2  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("claudio");
+        conta2.setAgencia("1234");
+        conta2.setBanco("Itau");
+        conta2.setNumero("1234");
+        conta2.setTitular("claudio");
 
         Conta conta3  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("roberto");
+        conta3.setAgencia("1234");
+        conta3.setBanco("Itau");
+        conta3.setNumero("1234");
+        conta3.setTitular("roberto");
 
         Conta conta4  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("pedro");
+        conta4.setAgencia("1234");
+        conta4.setBanco("Itau");
+        conta4.setNumero("1234");
+        conta4.setTitular("pedro");
 
         Conta conta5  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("paulo");
+        conta5.setAgencia("1234");
+        conta5.setBanco("Itau");
+        conta5.setNumero("1234");
+        conta5.setTitular("paulo");
 
         Conta conta6  = new Conta();
-        conta.setAgencia("1234");
-        conta.setBanco("Itau");
-        conta.setNumero("1234");
-        conta.setTitular("lucas");
+        conta6.setAgencia("1234");
+        conta6.setBanco("Itau");
+        conta6.setNumero("1234");
+        conta6.setTitular("lucas");
 
         EntityManager em = new JpaUtil().getEntityManager();
 
