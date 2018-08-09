9e8a97d570293903b9e92d4dfef00fb6af723949
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-09 09:21:45 -0300

    Comentario e mudanças na classe

--- banco/src/main/java/jpa/TodasAsMovimentacoes.java
+++ banco/src/main/java/jpa/TodasAsMovimentacoes.java
@@ -12,7 +12,18 @@ public class TodasAsMovimentacoes {
         EntityManager em = new JpaUtil().getEntityManager();
         em.getTransaction().begin();
 
-        String jpql = "select c from Conta c JOIN  fetch  c.movimentacoes";
+        /**
+         * A falta do uso do distinct faz com que cada conta que tenha mais que 1 movimentação,
+         * no caso a conta Paulo, se ela tiver 3 movimentaçoes ela será impressa 3 vezes,
+         * pois a Jpa faz um produto cartesiano
+         */
+
+        String jpql = "select distinct c from Conta c JOIN  fetch  c.movimentacoes";
+        /**
+         * usando o left join, busca-se todas as contas no banco inclusive as que não tem
+         * movimentação
+         * String jpql = "select distinct c from Conta c left JOIN  fetch  c.movimentacoes";
+         */
 
         Query query = em.createQuery(jpql);
 
@@ -23,6 +34,9 @@ public class TodasAsMovimentacoes {
             System.out.println("Movimentacoes: ");
            System.out.println(conta.getMovimentacoes());
         }
+
+        em.close();
+        JpaUtil.closeEMF();
 
     }
 
